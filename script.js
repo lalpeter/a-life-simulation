cfb7c14982012a49b71ae0622c900f615f3c1c24
L Peter <106273197+lalpeter@users.noreply.github.com>
2024-10-16 19:41:17 +0000

    Yes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const nodes = [500][500];
+
+function letLive(x, y){
+    let nbrs = 0;
+    if (x != 0 && nodes[y][x-1] == 1){
+        nbrs += 1;
+    }
+    if (x != 499 && nodes[y][x+1] == 1){
+        nbrs += 1;
+    }
+    if (y != 0 && nodes[y-1][x] == 1){
+        nbrs += 1;
+    }
+    if (y != 499 && nodes[y+1][x] == 1){
+        nbrs += 1;
+    }
+    return 0
+}
+
+function run(){
+    for(let y = 0; y < 500; y++){
+        for(let x = 0; x < 500; x++){
+            let px = nodes[y][x];
+        }
+    }
+}
+
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "red";
